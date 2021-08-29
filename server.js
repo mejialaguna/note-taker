51c912b51a97f1b898eb2b4f36e0c0dae140c833
Jose Mejia <mejialaguna@yahoo.com>
2021-08-29 13:22:48 -0400

    adding get route

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 const fs = require("fs");
 const path = require("path");
+var uniqid = require("uniqid");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.urlencoded({extended: true})) // : == to the value
+app.use(express.urlencoded({ extended: true })); // : == to the value
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/api/notes" , (req ,res) => {
-    // read file and send it to the array to the front end (db,json)
-    res.json()
-})
+app.get("/api/notes", (req, res) => {
+  // read file and send it to the array to the front end (db,json)
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    //JSON || Json.stringify = build in object into java script
+    res.json(JSON.parse(data));
+  });
+});
 
-app.post("/api/notes" , (req ,res) => {
- // read the note , write the note send file..
-    console.log(req.body);  
-})
 
-app.delete("/api/notes/:id" , (req ,res) => {
 
-    console.log(req.params.id)
-})
 
 
 // route to index.html
-app.get('/' , (req , res) =>{
-    res.sendFile(path.join(__dirname , "public/index.html"));
-})
-// route to notes.html
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(PORT , () => console.log("now listening on port 3001"));
+// route to notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.listen(PORT, () => console.log("now listening on port 3001"));
