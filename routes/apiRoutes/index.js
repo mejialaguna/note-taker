fb8029feb97ba3dc7a0c58420ed2168d0262b50a
Jose Mejia <mejialaguna@yahoo.com>
2021-08-29 14:59:13 -0400

    fixing routes issues

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,71 +1,71 @@
-const path = require("path");
-const fs = require('fs');
-var uniqid = require("uniqid");
-const router = require("express").Router();
+// const path = require("path");
+// const fs = require('fs');
+// var uniqid = require("uniqid");
+// const router = require("express").Router();
 
-router.get("/api/notes", (req, res) => {
-  // read file and send it to the array to the front end (db,json)
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    if (err) {
-      res.status(500).json(err);
-      return;
-    }
-    //JSON || Json.stringify = build in object into java script
-    res.json(JSON.parse(data));
-  });
-});
+// router.get("/api/notes", (req, res) => {
+//   // read file and send it to the array to the front end (db,json)
+//   fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+//     if (err) {
+//       res.status(500).json(err);
+//       return;
+//     }
+//     //JSON || Json.stringify = build in object into java script
+//     res.json(JSON.parse(data));
+//   });
+// });
 
-router.post("/api/notes", (req, res) => {
-  // read the note , write the note send file..
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    if (err) {
-      res.status(500).json(err);
-      return;
-    }
+// router.post("/api/notes", (req, res) => {
+//   // read the note , write the note send file..
+//   fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+//     if (err) {
+//       res.status(500).json(err);
+//       return;
+//     }
 
-    const oldNotes = JSON.parse(data);
+//     const oldNotes = JSON.parse(data);
 
-    let newNote = req.body;
-    newNote.id = uniqid();
-    // oldNotes.unshift(newNote);
+//     let newNote = req.body;
+//     newNote.id = uniqid();
+//     // oldNotes.unshift(newNote);
 
-    fs.writeFile(
-      path.join(__dirname, "db/db.json"),
-      JSON.stringify([newNote, ...oldNotes]),
-      (err) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        //file written successfully
-      }
-    );
-    res.json(oldNotes);
-  });
-});
+//     fs.writeFile(
+//       path.join(__dirname, "db/db.json"),
+//       JSON.stringify([newNote, ...oldNotes]),
+//       (err) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         //file written successfully
+//       }
+//     );
+//     res.json(oldNotes);
+//   });
+// });
 
-router.delete("/api/notes/:id", (req, res) => {
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    if (err) {
-      res.status(500).json(err);
-      return;
-    }
-    //JSON || Json.stringify = build in object into java script
-    let oldNotes = JSON.parse(data);
-    let newNotes = oldNotes.filter((note) => note.id !== req.params.id);
+// router.delete("/api/notes/:id", (req, res) => {
+//   fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+//     if (err) {
+//       res.status(500).json(err);
+//       return;
+//     }
+//     //JSON || Json.stringify = build in object into java script
+//     let oldNotes = JSON.parse(data);
+//     let newNotes = oldNotes.filter((note) => note.id !== req.params.id);
 
-    fs.writeFile(
-      path.join(__dirname, "db/db.json"),
-      JSON.stringify(newNotes),
-      (err) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-      }
-    );
-    res.json(newNotes);
-  });
-});
+//     fs.writeFile(
+//       path.join(__dirname, "db/db.json"),
+//       JSON.stringify(newNotes),
+//       (err) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//       }
+//     );
+//     res.json(newNotes);
+//   });
+// });
 
-module.exports = router; 
+// module.exports = router; 
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,15 @@
-const path = require("path");
-const router = require("express").Router();
+// const path = require("path");
+// const express = require("express")
+// const router = express.Router();
 
-// route to index.html
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
-});
+// // route to index.html
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
 
-// route to notes.html
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/notes.html"));
-});
+// // route to notes.html
+// router.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../public/notes.html"));
+// });
 
-module.exports = router;
+// module.exports = router;
