6cd9d31fd6eccdc20a348d41a6a68ebfb4649cb6
Jose Mejia <mejialaguna@yahoo.com>
2021-08-29 13:27:45 -0400

    all paths are functioning

--- server.js
+++ server.js
@@ -20,9 +20,58 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
+app.post("/api/notes", (req, res) => {
+  // read the note , write the note send file..
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
 
+    const oldNotes = JSON.parse(data);
 
+    let newNote = req.body;
+    newNote.id = uniqid();
+    // oldNotes.unshift(newNote);
 
+    fs.writeFile(
+      path.join(__dirname, "db/db.json"),
+      JSON.stringify([newNote, ...oldNotes]),
+      (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        //file written successfully
+      }
+    );
+    res.json(oldNotes);
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    //JSON || Json.stringify = build in object into java script
+    let oldNotes = JSON.parse(data);
+    let newNotes = oldNotes.filter((note) => note.id !== req.params.id);
+
+    fs.writeFile(
+      path.join(__dirname, "db/db.json"),
+      JSON.stringify(newNotes),
+      (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      }
+    );
+    res.json(newNotes);
+  });
+});
 
 // route to index.html
 app.get("/", (req, res) => {
